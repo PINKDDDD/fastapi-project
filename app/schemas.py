f69b4c55bf8498a5de6856b8126a3e8c7b990897
Zihui Yuan <75320084+PINKDDDD@users.noreply.github.com>
2023-04-04 17:13:42 -0500

    changedd

--- app/schemas.py
+++ app/schemas.py
@@ -40,12 +40,6 @@ class CreateUser(UserBase):
     pass
 
 
-class User(BaseModel):
-    id:int
-    email:EmailStr
-    created_at: datetime 
-    class Config:
-        orm_mode = True
 
 
 class Token(BaseModel):
